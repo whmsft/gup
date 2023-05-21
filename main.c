070bc136117477aafc65beb70b796794fbd7c622
whmsft <whmsft@outlook.com>
2023-05-21 17:55:24 +0530

    Refactor drag functionality for window movement

--- main.c
+++ main.c
@@ -7,24 +7,37 @@ int heightScreen = 450;
 float unit = (225/45);
 int clicks[] = {};
 
-int update(void) {}
+struct Drag {
+	int changeX, changeY;
+	int previousX, previousY;
+} WindowDrag;
+
+int update(void) {
+	WindowDrag.changeX = WindowDrag.previousX - GetMouseX();
+	WindowDrag.changeY = WindowDrag.previousY - GetMouseY();
+	
+	if (WindowDrag.changeX > 0 && WindowDrag.changeY > 0){
+		printf("Change: %d, %d\n", WindowDrag.changeX, WindowDrag.changeY);
+	}
+	if (IsMouseButtonDown(0)) {
+		SetWindowPosition(GetMouseX()+WindowDrag.changeX, GetMouseY()+WindowDrag.changeY);
+	}
+	WindowDrag.previousX = GetMouseX();
+	WindowDrag.previousY = GetMouseY();
+}
 
 int draw(void) {
-	for(int x = 0; x < 4; x++) {
+	for(int x = 0; x < 3; x++) {
 		for(int y = 0; y < 5; y++) {
 			DrawRectangle((unit*(10*x))+(unit*(1+x)), heightScreen-(unit*(11*y)), unit*10, unit*10, ColorFromHSV(0, 0, 0.2));
 		}
 	}
-	ColorFromHSV(0, 0, 0.2);
+	DrawRectangle(unit, heightScreen-unit*11, unit*21, unit*10, ColorFromHSV(0, 0, 0.153));
 }
 
 int main(void) {
 	InitWindow(widthScreen, heightScreen, "RayLib");
 	SetWindowState(FLAG_WINDOW_UNDECORATED);
-	Image icon = LoadImage("favicon.v1.ico");
-	ImageFormat(&icon, PIXELFORMAT_UNCOMPRESSED_R8G8B8A8);
-	SetWindowIcon(icon);
-	UnloadImage(icon);
 	
 	SetTargetFPS(60);
 	
